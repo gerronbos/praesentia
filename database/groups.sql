65d490e9b80a96bc3cc70f9ef9bb369ab0e61dc7
nsamoedj <nsamoedj@gmail.com>
2017-04-21 18:23:38 +0200

    Database

---
+++ database/groups.sql
@@ -0,0 +1,39 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 21 apr 2017 om 18:11
+-- Serverversie: 10.1.21-MariaDB
+-- PHP-versie: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `praesentia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `groups`
+--
+
+CREATE TABLE `groups` (
+  `ID` int(10) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `school_year` int(10) NOT NULL,
+  `period` int(10) NOT NULL,
+  `education_id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
