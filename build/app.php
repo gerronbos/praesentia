5a8345b7d06f3cfe5679e859bd6c72eb0f1afd96
Gerron Bos <gerronbos4@hotmail.com>
2017-04-24 13:56:37 +0200

    login klaar, sessionhandler klaar

--- build/app.php
+++ build/app.php
@@ -1,4 +1,6 @@
 <?php
+ob_start();
+
 /*
  * Config bestanden
  * Hier staan gegevens van de website in
@@ -15,6 +17,8 @@ require_once('../model/Users.php');
  * Services
  */
 require_once('../services/auth.php');
+require_once('../services/sessionhandler.php');
+
 /*
  * Repositories
  * Hier worden dingen berekent of opgehaald
@@ -24,4 +28,7 @@ require_once('../repositories/ConfigRepositorie.php');
 require_once('../repositories/MapStructureRepositorie.php');
 require_once('../repositories/FormRepositorie.php');
 
+
+
+Services\SessionHandler::save();
 ?>
---
+++ controller/logoutController.php
@@ -0,0 +1,6 @@
+<?php
+include('controller.php');
+
+Auth::logout();
+
+header('location:'.MapStructureRepositorie::view().'index.php');
