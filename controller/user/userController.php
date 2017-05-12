1191b63bb7344ed5f3ca12e64d5b8f1b7a38368c
Gerron Bos <gerronbos4@hotmail.com>
2017-05-12 14:50:05 +0200

    csv

--- controller/user/userController.php
+++ controller/user/userController.php
@@ -32,4 +32,34 @@
 		header("location:".MapStructureRepositorie::view()."user/allusers.php");
 		exit;
 	}
+    if(isset($_POST['csv'])){
+        if(isset($_FILES['file']['name']))
+            $target = MapStructureRepositorie::uploads();
+        $target = $target.basename($_FILES['file']['name']);
+
+
+        echo "<br />";
+
+        $file = fopen($_FILES['file']['tmp_name'],'r');
+        $data = array();
+        $header = null;
+        while(($entry = fgetcsv($file,'0',';')) !== FALSE){
+            if($header === null){
+                $header = $entry;
+                continue;
+            }
+            $data[] = array_combine($header,$entry);
+        }
+        fclose($file);
+
+        foreach($data as $row){
+            $lastname = $row['Tussenvoegsel'].' '.$row['Achternaam'];
+            $firstname = $row['Roepnaam'];
+            $user_number = $row['Stud.nr.'];
+            $email = $row['E-mailadres'];
+            $password = '123';
+
+            UserRepositorie::create($firstname, $lastname, $user_number, $email, $password);
+        }
+    }
 ?>
