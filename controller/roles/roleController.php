9d089236c6fd43cd5f3bec115d1004a5f958aae4
Gerron Bos <gerronbos4@hotmail.com>
2017-05-12 14:24:13 +0200

    bugfix

--- controller/roles/roleController.php
+++ controller/roles/roleController.php
@@ -7,7 +7,7 @@ include('../controller.php');
 if($_POST['create']){
     RoleRepository::create($_POST);
 
-    Services\SessionHandler::createSession('alert_roles','Rechten Sjabloon succesvol toegevoegd!');
+    Services\SessionHandler::setSession('alert_roles','Rechten Sjabloon succesvol toegevoegd!');
     Header('location:'.MapStructureRepositorie::view()."roles/index.php");
     exit;
 }
