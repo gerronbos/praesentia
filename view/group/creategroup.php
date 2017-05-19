f566959353285e633f8019b878242358da335653
nsamoedj <nsamoedj@gmail.com>
2017-05-20 01:19:26 +0200

    layout

--- view/group/creategroup.php
+++ view/group/creategroup.php
@@ -6,16 +6,26 @@ if (!Auth::user()->can('groups')) {
 }
 ?>
 
-<?php
+<div class="x_panel">
+	<div class="x_titel">
+		<h2>Groep aanmaken</h2>
+		<ul class="nav navbar-right panel_toolbox"></ul>
+		<div class="clearfix"></div>
+	</div>
 
-echo FormRepositorie::openForm(['url' => MapStructureRepositorie::controller(). 'group/groupController.php?create=1', 'file' => 1, 'method' => 'POST']);
-echo FormRepositorie::text('Naam', '',['name'=>'name', 'required' => 1]);
-echo FormRepositorie::text('Schooljaar', '', ['name'=>'school_year', 'required' => 1]);
-echo FormRepositorie::text('Periode', '', ['name'=>'period', 'required' => 1]);
-echo FormRepositorie::text('Educatienummer', '', ['name'=>'education_id', 'required' => 1]);
-echo FormRepositorie::formSaveButton("javascript:history.back()");
-echo FormRepositorie::closeForm();
-?>
+	<div class="x_content">
+		<?php
+
+		echo FormRepositorie::openForm(['url' => MapStructureRepositorie::controller(). 'group/groupController.php?create=1', 'file' => 1, 'method' => 'POST']);
+		echo FormRepositorie::text('Naam', '',['name'=>'name', 'required' => 1]);
+		echo FormRepositorie::text('Schooljaar', '', ['name'=>'school_year', 'required' => 1]);
+		echo FormRepositorie::text('Periode', '', ['name'=>'period', 'required' => 1]);
+		echo FormRepositorie::text('Educatienummer', '', ['name'=>'education_id', 'required' => 1]);
+		echo FormRepositorie::formSaveButton("javascript:history.back()");
+		echo FormRepositorie::closeForm();
+		?>
+	</div>
+</div>
 
 <?php
 include_once('../includes/footer.php');
