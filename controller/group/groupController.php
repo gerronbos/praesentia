bdcd7d949a40ecb04a62a42b7cdfee3f274c70ae
Jesse de Boer <jess.e@live.nl>
2017-05-23 15:17:06 +0200

    sgda

--- controller/group/groupController.php
+++ controller/group/groupController.php
@@ -4,7 +4,7 @@ if (!Auth::user()->can('groups')) {
     header("location: " . MapStructureRepositorie::error('401'));
     exit;
 }
-	if(isset($_POST['create'])){
+	if(isset($_GET['create'])){
 		GroupRepository::create($_POST['name'],$_POST['school_year'],$_POST['period'],$_POST['education_id']);
 		header("location:".MapStructureRepositorie::view()."group/creategroup.php");
 	}
