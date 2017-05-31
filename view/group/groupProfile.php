267ac956a594d2b315f30117d88ab9c01bcaa4f9
nsamoedj <nsamoedj@gmail.com>
2017-05-31 15:16:26 +0200

    fix

--- view/group/groupProfile.php
+++ view/group/groupProfile.php
@@ -1,8 +1,8 @@
 <?php include_once('../includes/head.php');
 $user = Services\SessionHandler::getSession('user_data');
 if (!Auth::user()->can('groups') || !Auth::user()->can('presence')) {
-    header("location: " . MapStructureRepositorie::error('401'));
-    exit;
+	header("location: " . MapStructureRepositorie::error('401'));
+	exit;
 }
 $group = model\Group::find($_GET['group_id']);
 $data2 = PresenceRepository::getByCourse(model\Course::find(1),['group_id'=>48]);
@@ -13,7 +13,9 @@ $data2 = PresenceRepository::getByCourse(model\Course::find(1),['group_id'=>48])
 	<div class="x_panel">
 		<div class="x_title">
 			<h2><?php echo $group->name ?></h2>
-			<?php echo "<a href'".MapStructureRepositorie::view()."group/allgroups.php' class='btn btn-primary' style='float: right;'>Terug naar alle groepen</a>" ?>
+			<?php
+			echo "<a href='".MapStructureRepositorie::view()."group/allgroups.php' class='btn btn-primary' style='float: right'>Terug naar alle groepen</a>";
+			?>
 			<div class="clearfix"></div>
 		</div>
 		<div class="x_content">
@@ -31,7 +33,7 @@ $data2 = PresenceRepository::getByCourse(model\Course::find(1),['group_id'=>48])
 					<div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
 						<table class="table table-bordered">
 							<tr>
-                                <th></th>
+								<th></th>
 								<th>Naam</th>
 								<th>Gebruikersnaam</th>
 								<th>Email</th>
@@ -42,16 +44,16 @@ $data2 = PresenceRepository::getByCourse(model\Course::find(1),['group_id'=>48])
 								$data = PresenceRepository::calcPresenceByUser($user,['grouped' => 1]);
 								echo "<tr><td style='text-align: center'><img width='60px' src='".$user->getUserProfilePicture()."'></td><td>".$user->fullnameReturned(['url'=>1])."</td><td>$user->user_number</td><td>$user->email</td><td>";
 								echo progressBar($data['amount_present_prec'],['item_id'=>$user->id])."</tr>";
-						}
-						?>
-                        </table>
-					    <a target="_blank" href="<?php echo MapStructureRepositorie::Controller() ?>pdfController.php?GroupSummary=1&gid=<?php echo $group->id ?>" class="btn btn-primary pull-right"><i class="fa fa-download"></i> Opslaan als PDF</a>
-				    </div>
+							}
+							?>
+						</table>
+						<a target="_blank" href="<?php echo MapStructureRepositorie::Controller() ?>pdfController.php?GroupSummary=1&gid=<?php echo $group->id ?>" class="btn btn-primary pull-right"><i class="fa fa-download"></i> Opslaan als PDF</a>
+					</div>
 
-			    </div>
-		    </div>
-	    </div>
-    </div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 
@@ -59,7 +61,7 @@ $data2 = PresenceRepository::getByCourse(model\Course::find(1),['group_id'=>48])
 $data = PresenceRepository::calcPresenceByGroup($group);
 ?>
 <script>
-    var onClickUrl = '<?php echo MapStructureRepositorie::view()."lecture/presence/presence.php?group_id=$group->id&course_name=:name";?>';
+	var onClickUrl = '<?php echo MapStructureRepositorie::view()."lecture/presence/presence.php?group_id=$group->id&course_name=:name";?>';
 	var labels = [];
 	var data = [];
 	<?php foreach($data as $d){
@@ -68,7 +70,7 @@ $data = PresenceRepository::calcPresenceByGroup($group);
 		echo "data.push('".$d['amount_present_prec']."');";
 	}
 	?>
-    var canvas = document.getElementById("myChart");
+	var canvas = document.getElementById("myChart");
 	var ctx = canvas.getContext("2d");
 	var myChart = new Chart(ctx, {
 		type: 'bar',
@@ -88,17 +90,17 @@ $data = PresenceRepository::calcPresenceByGroup($group);
 			}]
 		},
 		options: {
-            scales: {
-                yAxes: [{
-                    display: true,
-                    ticks: {
-                        beginAtZero: true,
-                        steps: 10,
-                        stepValue: 10,
-                        max: 100
-                    }
-                }]
-            },
+			scales: {
+				yAxes: [{
+					display: true,
+					ticks: {
+						beginAtZero: true,
+						steps: 10,
+						stepValue: 10,
+						max: 100
+					}
+				}]
+			},
 			tooltips: {
 				enabled: true,
 				mode: 'single',
@@ -113,22 +115,22 @@ $data = PresenceRepository::calcPresenceByGroup($group);
 
 	});
 
-    canvas.onclick = function (evt) {
-        var activePoints = myChart.getElementsAtEvent(evt);
-        var chartData = activePoints[0]['_chart'].config.data;
-        var idx = activePoints[0]['_index'];
+	canvas.onclick = function (evt) {
+		var activePoints = myChart.getElementsAtEvent(evt);
+		var chartData = activePoints[0]['_chart'].config.data;
+		var idx = activePoints[0]['_index'];
 
-        var label = chartData.labels[idx];
-        var url = onClickUrl.replace(':name',label);
-        window.location.href = url;
+		var label = chartData.labels[idx];
+		var url = onClickUrl.replace(':name',label);
+		window.location.href = url;
 
-    };
-    $(document).ready(function(){
-        var url = '<?php echo MapStructureRepositorie::controller()."user/userController.php?show=1&user_id=".$user->id ?>';
-        $('.progress-bar').click(function(){
+	};
+	$(document).ready(function(){
+		var url = '<?php echo MapStructureRepositorie::controller()."user/userController.php?show=1&user_id=".$user->id ?>';
+		$('.progress-bar').click(function(){
             //do something
             var courseId = $(this).attr('item_id');
             window.location.href = url.replace(':id', courseId);
         });
-    });
+	});
 </script>
