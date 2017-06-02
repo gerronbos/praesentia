72c8ef3808eed3964b802fedd6e21a9abe516199
Tom van den Noort <tvdn24@gmail.com>
2017-06-02 14:59:15 +0200

    Tiny fix in password reset window... it only needed just a paintbrush

--- view/user/resetPassword.php
+++ view/user/resetPassword.php
@@ -82,9 +82,85 @@ $user = model\Users::where('id','=',$upr->user_id)->first();
 	</div>
 </div>
 </div>
-<?php
-include_once('../includes/footer.php');
-?>
+
+</div>
+<!-- /page content -->
+
+<!-- footer content -->
+<footer style="margin:0;">
+    <div class="pull-right">
+        <?php echo ConfigRepositorie::get('title'); ?> - Aanwezigheids administratie
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+
+<!-- jQuery -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>skycons/skycons.js"></script>
+<!-- Flot -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>Flot/jquery.flot.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>Flot/jquery.flot.pie.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>Flot/jquery.flot.time.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>Flot/jquery.flot.stack.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>jqvmap/dist/jquery.vmap.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>moment/min/moment.min.js"></script>
+<script src="<?php echo MapStructureRepositorie::vendors(); ?>bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Password Validator -->
+<script src="<?php echo MapStructureRepositorie::build(); ?>js/validator.js"></script>
+
+<!-- Dropzone Script -->
+<script src="<?php echo MapStructureRepositorie::build(); ?>js/dropzone.js"></script>
+
+<!-- Select2 CDN -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="<?php echo MapStructureRepositorie::build(); ?>js/custom.min.js"></script>
+
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $(".select2").select2({
+            placeholder: "Selecteer"
+        });
+    });
+</script>
+
+</body>
+</html>
+
 <script>
 	$("#password").keyup(function(){
 		$.ajax({
